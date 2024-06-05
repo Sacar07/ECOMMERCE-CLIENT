ab9c143bd4fcbf8b10f45ce4460fdb5665e51ea6
Sacar <sakarstha42@gmail.com>
2024-06-06 00:01:18 +0545

    feat:latest-products

---
+++ src/components/home/LatestProducts.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// export default function LatestProducts() {
+//   return (
+//     <>
+//     <div className="container">
+//         {
+//             [1,2,3,4].map((el) => {
+
+//             })
+//         }
+//     </div>
+//     </>
+//   )
+// }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -87,13 +87,48 @@ export default function Home() {
       </Slider>
       <div className="container grid gap-[30px] py-[125px] sm:py-[140px] md:grid-cols-2 md:py-[158px] lg:grid-cols-4 lg:py-[178px] xl:py-[200px] xxl:py-[226px] ">
         {products.map((el) => {
-          return <TrendingProducts
-          image= {el.image}
-          code= {el.code}
-          price = {el.price}
-          name = {el.name}
-          />;
+          return (
+            <TrendingProducts
+              image={el.image}
+              code={el.code}
+              price={el.price}
+              name={el.name}
+            />
+          );
         })}
+      </div>
+      <div className="container">
+        <p className="text-center font-josefin text-[30px] font-bold text-[#151875] sm:text-[42px]">
+          Latest Products
+        </p>
+
+        <ul className="mt-[19px] flex justify-center gap-[15px] font-lato text-[14px] text-[#151875] sm:gap-[60px] sm:text-[18px]">
+          <li className="text-secondary underline">New Arrival</li>
+          <li>Best Seller</li>
+          <li>Featured</li>
+          <li>Special Offer</li>
+        </ul>
+
+        <div className="pt-[58px]  grid md:grid-cols-2 lg:grid-cols-3 gap-[116px] ">
+          {[1, 2, 3, 4, 5, 6].map((el) => {
+            return (
+              <div className="h-[301px] w-[340px] bg-primary-light  shadow-[0px_0px_25px_0px_rgba(0,0,0,0.1)] sm:w-[360px]">
+                <img
+                  className="mx-auto mt-[33px]  "
+                  src="/assets/latest-1.png"
+                  alt=""
+                />
+                <div className="flex justify-between bg-white p-[8px] font-josefin text-[16px]">
+                  <p>Comfort Handy Craft</p>
+                  <div className="mr-[6px] flex gap-[10px] text-[14px]">
+                    <p>$42.00</p>
+                    <p className="text-secondary line-through">$65.00</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
