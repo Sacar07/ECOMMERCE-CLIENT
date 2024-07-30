c299526acba3d314a146791f45e30466e58bfee5
Sacar <sakarstha42@gmail.com>
2024-07-30 17:19:08 +0545

    feat: implement cart checkout with total amount and add shipping charge

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -32,27 +32,27 @@ export default function Cart() {
   return (
     <>
       {/* {JSON.stringify(cart)} */}
+      {console.log("cart is", cart)}
       <Breadcrumb title="Shopping Cart" subtext="pages" />
       <div className="container mt-[130px]">
         <table className=" border-collapse  border-spacing-2 ">
           <thead>
-            <tr className="font-josefin ">
+            <tr className="font-josefin  text-[#1D3178]">
               <th className="p-2">Product</th>
               <th className="columns-2 p-2">Price</th>
               <th className="columns-2 p-2">Quantity</th>
-              <th className="p-2">Total</th>
-
-              {/* <th className="border border-slate-600">Actions</th> */}
+              <th className=" p-2">Total</th>
+              {/* <th className="p-2 ">Cart Totals</th> */}
             </tr>
           </thead>
           {cart?.map((el) => {
             return (
               <tbody className="space-y-10 ">
-                <tr key={el._id} className="p-4 ">
+                <tr key={el._id} className="relative border-b p-4">
                   <td className=" columns-2 p-[10px] ">
-                    <div className="relative flex  gap-[16px] ">
+                    <div className=" flex  gap-[16px] ">
                       <img className="size-[84px]" src={el.image} alt="" />
-                      <div className="absolute left-[74px] top-0 cursor-pointer">
+                      <div className="absolute left-[84px] top-0 cursor-pointer">
                         <MdCancel
                           onClick={() => dispatch(removeCartItem(el._id))}
                         />
@@ -98,10 +98,45 @@ export default function Cart() {
                     </div>
                   </td>
                 </tr>
-                <hr />
               </tbody>
             );
           })}
+        </table>
+        <table className="absolute left-[1000px] top-[547px]">
+          <thead>
+            <tr className=" p-[34px]">
+              <th className="pb-[42px] text-[#1D3178]">Cart Totals</th>
+            </tr>
+          </thead>
+          <tbody className="  bg-[#E8E6F1]">
+            <tr className="border-b border-b-slate-300  pt-[34px]">
+              <td className="px-4 py-2 pt-[34px] ">Subtotals:</td>
+              <td className="px-4 py-2 pt-[34px] ">
+                $ {cart.reduce((acc, el) => acc + el.price * el.quantity, 0)}
+              </td>
+            </tr>
+            <tr className="border-b border-b-slate-300">
+              <td className="px-4 py-2 pt-[34px] ">Totals:</td>
+              <td className="px-4 py-2  pt-[34px]">
+                $
+                {cart.reduce((acc, el) => acc + el.price * el.quantity, 0) +
+                  100}
+              </td>
+            </tr>
+            <tr className="">
+              <td className="bg-[#E8E6F1] px-4  py-2   pt-[27px] font-lato text-[12px] text-[#8A91AB] ">
+                shipping and taxes calculated at checkout
+              </td>
+              <td> </td>
+            </tr>
+            <button className="btn mb-[32px] ml-20  mt-[34px] px-4  py-2">
+              Proceed To Checkout
+            </button>
+            <td></td>
+          </tbody>
+          {/* <span className="bg-[#E8E6F1] px-4   py-2 font-lato text-[12px] text-[#8A91AB] ">
+            shipping and taxes calculated at checkout
+          </span> */}
         </table>
 
         <button
